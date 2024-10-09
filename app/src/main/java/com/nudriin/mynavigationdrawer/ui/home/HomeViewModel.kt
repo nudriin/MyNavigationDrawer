acc8adeb9734bcc976bd3b1afa37cf40eae1b441
Nurdin <mrsunnysummer@gmail.com>
2024-10-10 01:08:31 +0700

    feat: create cart fragment and navigation functionality

---
+++ app/src/main/java/com/nudriin/mynavigationdrawer/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.nudriin.mynavigationdrawer.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
